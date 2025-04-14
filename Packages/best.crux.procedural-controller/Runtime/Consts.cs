336c244d82528d198c98d38ef89fc9a59762397b
Fen Dweller <chemicalcrux@gmail.com>
2025-04-14 12:02:33 -0500

    Add a new icon

---
+++ Packages/best.crux.procedural-controller/Runtime/Consts.cs
@@ -0,0 +1,9 @@
+using Crux.Core.Runtime;
+
+namespace Crux.ProceduralController.Runtime
+{
+    internal static class Consts
+    {
+        internal const string ComponentRootPath = CoreConsts.ComponentRootPath + "Procedural Controller/";
+    }
+}
--- Packages/best.crux.procedural-controller/Runtime/ProceduralControllerBuilder.cs
+++ Packages/best.crux.procedural-controller/Runtime/ProceduralControllerBuilder.cs
@@ -3,6 +3,7 @@ using VRC.SDKBase;
 
 namespace Crux.ProceduralController.Runtime
 {
+    [AddComponentMenu(menuName: Consts.ComponentRootPath + "Procedural Controller")]
     public class ProceduralControllerBuilder : MonoBehaviour, IEditorOnly
     {
         [SerializeField, SerializeReference] internal ProceduralControllerData data = new ProceduralControllerDataV1();
