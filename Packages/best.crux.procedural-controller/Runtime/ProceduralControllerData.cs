8295dc5fb0d5ecbd6ee960d171d4ebe43ad2a36c
Fen Dweller <chemicalcrux@gmail.com>
2025-09-17 11:28:31 -0500

    Add tooltips

--- Packages/best.crux.procedural-controller/Runtime/ProceduralControllerData.cs
+++ Packages/best.crux.procedural-controller/Runtime/ProceduralControllerData.cs
@@ -23,7 +23,9 @@ namespace Crux.ProceduralController.Runtime
         [TooltipRefInert("62be95ed7dd0a4c65bcbb3afd79ab524,9197481963319205126")]
         [SerializeField] internal DecoratedList<ComponentModel> componentModels;
 
+        [TooltipRef("d79fedb36e6c5494b888c685b11c0de0,9197481963319205126")]
         [SerializeField] internal string menuPrefix;
+        [TooltipRef("49cbbbb2739404b16b87236f02f5bbf7,9197481963319205126")]
         [SerializeField] internal Texture2D menuIcon;
         
         public override ProceduralControllerData Upgrade()
