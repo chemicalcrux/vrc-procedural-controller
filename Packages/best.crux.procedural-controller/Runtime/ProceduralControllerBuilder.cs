8c47d871bed7fe3d15076f4b14018e47111fbefc
Fen Dweller <chemicalcrux@gmail.com>
2025-10-16 12:19:30 -0500

    Add the HideIcon attribute to ProceduralControllerBuilder

--- Packages/best.crux.procedural-controller/Runtime/ProceduralControllerBuilder.cs
+++ Packages/best.crux.procedural-controller/Runtime/ProceduralControllerBuilder.cs
@@ -1,9 +1,11 @@
+using Crux.Core.Runtime.Attributes;
 using UnityEngine;
 using VRC.SDKBase;
 
 namespace Crux.ProceduralController.Runtime
 {
     [AddComponentMenu(menuName: Consts.ComponentRootPath + "Procedural Controller")]
+    [HideIcon]
     public class ProceduralControllerBuilder : MonoBehaviour, IEditorOnly
     {
         [SerializeField, SerializeReference] internal ProceduralControllerData data = new ProceduralControllerDataV1();
